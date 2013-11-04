f9e0f6f97a739836942d0110cb7c524438ca89d9
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-03 19:59:57 -0500

    fixing mult test

--- test/mult.c
+++ test/mult.c
@@ -7,7 +7,7 @@ int main(void) {
   int a, b, prod;
   plan(3);
 
-  ok(mult(1, 2) == 1, "1 * 1 == 1");
+  ok(mult(1, 1) == 1, "1 * 1 == 1");
   ok(mult(1, 2) == 2, "1 * 2 == 2");
   ok(mult(2, 3) == 6, "2 * 3 == 6");
   return 0;
